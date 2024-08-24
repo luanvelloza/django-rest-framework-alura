e32fca0bacae7ec4850a7c67afbc10527c1cc3d8
luanvelloza <luanvelloza@gmail.com>
2024-08-24 19:54:53 -0300

    feat: added in admin' config -  models: course and student

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display= ('id', 'name', 'email', 'cpf', 'birthday_date', 'phone_number')
+    list_display_links = ('id','name',)
+    list_per_page = 20
+    search_fields = ('name',)
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description')
+    list_display_links = ('id', 'code',)
+    search_fields = ('code',)
+
+admin.site.register(Course, Courses)
+
