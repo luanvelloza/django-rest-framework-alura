445996f544e09b73a49b709f49c84d57eef2624e
luanvelloza <luanvelloza@gmail.com>
2024-08-29 21:42:25 -0300

    feat: added model: Enrollment

--- school/models.py
+++ school/models.py
@@ -16,12 +16,26 @@ class Course(models.Model):
     LEVEL = {
         ('B', 'Básico'),
         ('I', 'Intermediário'),
-        ('A', 'Avançado')
+        ('A', 'Avançado'),
     }
 
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=100, blank=False)
     level =models.CharField(max_length=1, blank=False, choices=LEVEL, default='B', null=False)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.code
+
+
+class Enrollment(models.Model):
+
+    PERIOD = {
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N','Noturno'),
+    }
+
+    student = models.ForeignKey(Student, on_delete= models.CASCADE)
+    course = models.ForeignKey(Course, on_delete= models.CASCADE)
+
+    period =models.CharField(max_length=1, blank=False, choices=PERIOD, default='M', null=False)
