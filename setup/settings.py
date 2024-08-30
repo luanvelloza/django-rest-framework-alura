aff8d39904136bd5c6e79703ec33250db2138aa7
luanvelloza <luanvelloza@gmail.com>
2024-08-29 21:50:11 -0300

    feat: Added global authentication with the IsAuthenticated permission.

--- setup/settings.py
+++ setup/settings.py
@@ -127,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest Freamework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
